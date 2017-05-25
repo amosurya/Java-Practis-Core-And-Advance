766a0985c08d4168b7aef3bffc63b57ff7d074d1
Suryavanshi <amol.suryavanshi@capgemini.com>
2017-05-25 14:15:35 +0530

    Java workspace

--- Core Java Practis/src/com/thread/CallableThreadmain.java
+++ Core Java Practis/src/com/thread/CallableThreadmain.java
@@ -1,10 +1,16 @@
 package com.thread;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -35,6 +41,21 @@ public class CallableThreadmain {
 	            list.add(future);
 	            System.out.println(new Date()+ "::"+(Future<String>)future);
 	        }
+			
+			try {
+	            File file = new File("test.txt");
+	            FileInputStream fis = new FileInputStream(file);
+	            BufferedReader bReader = new BufferedReader(new InputStreamReader(fis));
+	            String str = bReader.readLine();
+	            SimpleDateFormat format = new SimpleDateFormat("DD/MM/YY");
+	            Date date = format.parse(str);
+
+	
+			 
+			}catch(Exception  ex) {
+				   // handle ParseException and IOException here.
+			}
+
 			
 		
 			
